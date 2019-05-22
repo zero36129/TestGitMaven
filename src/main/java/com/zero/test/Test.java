a3f47c37b6d2838bcdeada0d52a6733c7ae1a9dd
zero36129 <249851228@qq.com>
2019-05-22 16:19:45 +0800

    why error？

--- src/main/java/com/zero/test/Test.java
+++ src/main/java/com/zero/test/Test.java
@@ -4,7 +4,7 @@ public class Test {
     
     public static void main(String[] args) {
         System.out.println("This is my test program.");
-        System.out.println("second");
+        System.out.println("second 111");
     }
 
 }
